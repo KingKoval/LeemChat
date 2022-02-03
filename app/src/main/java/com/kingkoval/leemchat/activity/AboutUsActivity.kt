17fcee4f78736e07d3bc5e38105e44159c0e8b37
KingKoval <kovalchuk.b14@gmail.com>
2022-02-03 16:30:05 +0100

    added AboutUsActivity.kt

---
+++ app/src/main/java/com/kingkoval/leemchat/activity/AboutUsActivity.kt
@@ -0,0 +1,12 @@
+package com.kingkoval.leemchat.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kingkoval.leemchat.R
+
+class AboutUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}

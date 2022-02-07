013ea2f6e77c8f7236d20acd7181cc71f295d633
KingKoval <kovalchuk.b14@gmail.com>
2022-02-07 17:37:30 +0100

    added argument 'timeMessage'

--- app/src/main/java/com/kingkoval/leemchat/model/Message.kt
+++ app/src/main/java/com/kingkoval/leemchat/model/Message.kt
@@ -2,5 +2,6 @@ package com.kingkoval.leemchat.model
 
 data class Message(
     var message: String = "",
-    var senderUid: String = ""
+    var senderUid: String = "",
+    var timeMessage: Any? = null
 )

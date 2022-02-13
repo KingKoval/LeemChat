0e29d525683cd614e2eb10bdf83ce11c7e9f9118
KingKoval <kovalchuk.b14@gmail.com>
2022-02-13 16:25:07 +0100

    deleted function convertTime()

--- app/src/main/java/com/kingkoval/leemchat/activity/ChatActivity.kt
+++ app/src/main/java/com/kingkoval/leemchat/activity/ChatActivity.kt
@@ -1,8 +1,17 @@
 package com.kingkoval.leemchat.activity
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
@@ -13,6 +22,8 @@ import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.activity_chat.iv_user_photo
 import kotlinx.android.synthetic.main.activity_chat.tv_user_name
 import com.kingkoval.leemchat.model.Message
+import kotlinx.android.synthetic.main.activity_registration.*
+import kotlinx.android.synthetic.main.send_message_item.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -27,6 +38,32 @@ class ChatActivity : AppCompatActivity() {
 
     lateinit var dbRef: DatabaseReference
     lateinit var  messageAdapter: MessageAdapter
+
+    lateinit var imageUri: Uri
+
+    val resultLauncherUploadImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result: ActivityResult ->
+        if(result.resultCode == RESULT_OK && result.data != null){
+            //checkUserPhoto = true
+            Log.i("IMAGE!!!", "Image was selected")
+            imageUri = result.data!!.data!!
+
+
+            val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, imageUri))
+            } else {
+                MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+            }
+
+            ib_send.setOnClickListener {
+                iv_send_image.background = BitmapDrawable(bitmap)
+            }
+
+        } else{
+            //checkUserPhoto = false
+        }
+
+    }
 
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,9 +91,17 @@ class ChatActivity : AppCompatActivity() {
 
 
         getMessageFromDatabase()
+        recycler_view_messages.scrollToPosition(messageAdapter.itemCount-1)
 
         et_message.setOnClickListener {
             recycler_view_messages.scrollToPosition(messageAdapter.itemCount-1)
+        }
+
+        ib_image.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+
+            resultLauncherUploadImage.launch(intent)
         }
 
         ib_send.setOnClickListener {
@@ -77,6 +122,7 @@ class ChatActivity : AppCompatActivity() {
                     dbRef.child(receiverRoom!!).child("messages").push()
                         .setValue(messageObject)
                 }
+
         }
     }
 
